9b3530b0fb24912efc124f2fdf1b0924ca1eb57b
firasbelhiba <firas.belhiba@esprit.tn>
2022-01-03 10:27:36 +0100

    Setting the template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,127 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <div className="overflow-hidden">
+        <header className="header__1 js-header" id="header">
+          <div className="container">
+            <div className="wrapper js-header-wrapper">
+              <div className="header__logo">
+                <Link href="/">
+                  <a>
+                    <img
+                      className="header__logo"
+                      id="logo_js"
+                      src="assets/img/logos/Logo.svg"
+                      alt="logo"
+                    />
+                  </a>
+                </Link>
+              </div>
+              <div className="header__menu">
+                <ul className="d-flex space-x-20">
+                  <li>
+                    <Link href="/Create">
+                      <a className="color_black"> Create</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/my-assets">
+                      <a className="color_black"> My digital assets</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/creator-dashboard">
+                      <a className="color_black"> Creator dashboard</a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              {/* ================= */}
+              <div className="header__search">
+                <input type="text" placeholder="Search" />
+                <button className="header__result">
+                  <i className="ri-search-line" />
+                </button>
+              </div>
+              <div className="header__btns">
+                <a className="btn btn-grad btn-sm" href="Connect-wallet.html">
+                  <i className="ri-wallet-3-line" />
+                  Connect wallet
+                </a>
+                <a href="#" id="connectbtn">
+                  <img width={45} src="assets/img/icons/metamask.svg" alt="" />
+                </a>
+              </div>
+              <div className="header__burger js-header-burger" />
+              <div className="header__mobile js-header-mobile">
+                <div className="header__mobile__menu space-y-40">
+                  <ul className="d-flex space-y-20">
+                    <li>
+                      {" "}
+                      <a className="color_black" href="Marketplace.html">
+                        {" "}
+                        Marketplace
+                      </a>{" "}
+                    </li>
+                    <li>
+                      {" "}
+                      <a className="color_black" href="Collections.html">
+                        {" "}
+                        Collections
+                      </a>{" "}
+                    </li>
+                    <li>
+                      {" "}
+                      <a className="color_black" href="Profile.html">
+                        {" "}
+                        Profile
+                      </a>{" "}
+                    </li>
+                    <li>
+                      {" "}
+                      <a className="color_black" href="Creators.html">
+                        {" "}
+                        Creators
+                      </a>{" "}
+                    </li>
+                  </ul>
+                  <div className="space-y-20">
+                    <div className="header__search in_mobile w-full">
+                      <input type="text" placeholder="Search" />
+                      <button className="header__result">
+                        <i className="ri-search-line" />
+                      </button>
+                    </div>
+                    <a
+                      className="btn btn-grad btn-sm"
+                      href="Connect-wallet.html"
+                    >
+                      Connect wallet
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+      <Component {...pageProps} />
+      <script src="assets/js/jquery-3.6.0.js"></script>
+      <script src="assets/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/js/swiper-bundle.min.js"></script>
+      <script src="assets/js/gsap.min.js"></script>
+      <script src="assets/js/ScrollTrigger.min.js"></script>
+      <script src="assets/js/sticky-sidebar.js"></script>
+      <script src="assets/js/script.js"></script>
+      <script src="../cdn.jsdelivr.net/npm/web3%40latest/dist/web3.min.js"></script>
+      <script src="../unpkg.com/moralis%400.0.178/dist/moralis.js"></script>
+      <script src="assets/js/nft.js"></script>
+      <script src="../unpkg.com/%40lottiefiles/lottie-player%401.5.4/dist/lottie-player.js"></script>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,49 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="title" content="Raroin" />
+          <meta name="description" content="Premium NFT marketplace theme" />
+          <meta
+            name="keywords"
+            content="bootstrap, creabik, ThemeForest, bootstrap5, agency theme, saas
+			theme, sass, html5"
+          />
+          <meta name="robots" content="index, follow" />
+          <meta name="language" content="English" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title> NFT Dungeon </title>
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          {/* 🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈 STYLES */}
+          <link rel="stylesheet" href="assets/css/plugins/remixicon.css" />
+          <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css" />
+          <link
+            rel="stylesheet"
+            href="assets/css/plugins/swiper-bundle.min.css"
+          />
+          <link rel="stylesheet" href="assets/css/style.css" />
+        </Head>
+        <body className="body">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
