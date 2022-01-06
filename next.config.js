bc825ee2558732fde4c10e3c8861bf4e2fc6e7de
firasbelhiba <firas.belhiba@esprit.tn>
2022-01-06 07:17:59 +0100

    Fixing errors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
+// module.exports = {
+//   reactStrictMode: true,
+// }
+
+/**
+ * @type {import('next').NextConfig}
+ */
+
+ const nextConfig = {
+  webpack: (config, { webpack }) => {
+      config.plugins.push(new webpack.IgnorePlugin({
+          resourceRegExp: /^web3Modal$/
+      }),);
+      return config
+  }
 }
+
+module.exports = nextConfig
